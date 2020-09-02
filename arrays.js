91eb962b0e12426c52135f8fcce6389ca565a31a
looping-torrent-8105 <gianherrera96@gmail.com>
2020-09-02 22:54:49 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,48 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var addCholate = 'Magnum';
+
+//Esta función inserta el elemento al principio de la matriz.
+function addElementToBeginningOfArray (chocolateBars,addCholate)
+{
+  return [addCholate,...chocolateBars];
+}
+
+/*Esta función nuevamente inserta el elemento al principio de la matriz, pero utilizando el método  "unshift" */
+function destructivelyAddElementToBeginningOfArray(chocolateBars,addCholate)
+{
+ chocolateBars.unshift(addCholate);
+ return chocolateBars;
+}
+
+//Esta función inserta el elemento al final de la matriz.
+function addElementToEndOfArray(chocolateBars,addCholate)
+{
+  return[...chocolateBars,addCholate];
+
+}
+/*Esta función nuevamente inserta el elemento al final de la matriz, pero utilizando el método  "push" */
+function destructivelyAddElementToEndOfArray(chocolateBars,addCholate)
+{
+  chocolateBars.push(addCholate);
+  return chocolateBars;
+}
+
+//Esta función tiene como objetivo acceder a un elemento de la matriz llamda "chocolateBars" por medio de un índice.
+function accessElementInArray(chocolateBars,index)
+{
+  return chocolateBars[index];
+}
+
+//Esta función elimina el primer elemento de la matriz "chocolateBars".
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars)
+{
+  chocolateBars.shift();
+  return chocolateBars;
+}
+
+//Esta función elimina el primer elemento de la matriz con el método de "slice".
+function removeElementFromBeginningOfArray(chocolateBars)
+{
+ chocolate = chocolateBars.slice(1);
+ return chocolate;
+}
